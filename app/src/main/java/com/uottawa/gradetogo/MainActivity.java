3b6859a99328c2b8fbf7ec458c1c1696fb741152
osfalos <ychah102@uottawa.ca>
2017-07-08 11:19:23 -0400

    download the project and modify the nav bar

--- app/src/main/java/com/uottawa/gradetogo/MainActivity.java
+++ app/src/main/java/com/uottawa/gradetogo/MainActivity.java
@@ -82,15 +82,12 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_help) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
