645726cad4c78d4cc6ccd3e2d451328a3d0fab29
osfalos <ychah102@uottawa.ca>
2017-07-12 22:57:30 -0400

    setting

--- app/src/main/java/com/uottawa/gradetogo/Singleton.java
+++ app/src/main/java/com/uottawa/gradetogo/Singleton.java
@@ -13,7 +13,7 @@ public class Singleton {
     //variables
     private static Singleton singleton; // l'instance du Singleton
     private ArrayList<Semester> semesters;
-
+    private int University;
     //les photos par defauts
     private Uri defaultImageA = Uri.parse("android.resource://com.uottawa.gradetogo/drawable/img_default");
     private Uri defaultImageB = Uri.parse("android.resource://com.uottawa.gradetogo/drawable/img_default2");
@@ -30,7 +30,7 @@ public class Singleton {
         semesters.get(0).addCourse(new Course("CSI 3581", 90, defaultImageA ));
         semesters.get(0).addCourse(new Course("SEG 3685", 100, defaultImageB));
         semesters.get(0).addCourse(new Course("SEG 3585", 70, defaultImageC));
-
+        University  = 0;
 
     }
 
@@ -42,8 +42,48 @@ public class Singleton {
         return singleton;
 
     }
+    public void setUniversity(int university) {
+        this.University = university;
+    }
 
+    public int getUniversity() {
+        return University;
+    }
 
+    public String getGrade(int perc){
+        int uni = getUniversity();
+        switch (uni) {
+            case 0 :
+                if(perc > 90){
+                    return "A+";
+                }
+                else if(perc >= 85 && perc <90){return "A";}
+                else if(perc >= 80 && perc <85){return "A-";}
+                else if(perc >= 75 && perc <80){return "B+";}
+                else if(perc >= 70 && perc <75){return "B";}
+                else if(perc >= 65 && perc <70){return "C+";}
+                else if(perc >= 60 && perc <65){return "C";}
+                else if(perc >= 55 && perc <60){return "D";}
+                else if(perc >= 50 && perc <55){return "F";}
+                else{return "E";}
+            case 1 :
+                if(perc > 90){
+                    return "A+";
+                }
+                else if(perc >= 85 && perc <90){return "A";}
+                else if(perc >= 80 && perc <85){return "A-";}
+                else if(perc >= 75 && perc <80){return "B+";}
+                else if(perc >= 70 && perc <75){return "B";}
+                else if(perc >= 65 && perc <70){return "B-";}
+                else if(perc >= 60 && perc <65){return "C+";}
+                else if(perc >= 55 && perc <60){return "C";}
+                else if(perc >= 50 && perc <55){return "D";}
+                else{return "E";}
+            case  2 :
+                break;
+        }
+        return "error";
+    }
 
     public ArrayList<Semester> getSemesters() {
         return semesters;
