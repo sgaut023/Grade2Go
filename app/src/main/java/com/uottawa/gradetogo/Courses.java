7487781b4eaa815691604d6041497dcbc4b706de
shanelgauthier <sgaut023@uottawa.ca>
2017-07-14 23:06:02 -0400

    Testing push

--- app/src/main/java/com/uottawa/gradetogo/Courses.java
+++ app/src/main/java/com/uottawa/gradetogo/Courses.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -44,8 +43,8 @@ public class Courses extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(Courses.this, AddCourse.class);
+                startActivity(i);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
